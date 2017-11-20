d776141ac2c5c2f37d4d573a7e3fc3f69630f913
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-11-20 18:32:54 +0100

    Copy provided and previous projects utilities in a single file

---
+++ utils.py
@@ -0,0 +1,105 @@
+import matplotlib.image as mpimg
+import numpy as np
+import os, sys
+import tensorflow as tf
+
+from PIL import Image
+
+
+def load_image(filename):
+    return mpimg.imread(filename)
+
+def img_float_to_uint8(img):
+    rimg = img - np.min(img)
+    rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8) # Pass from [0,1]f to [0,255]i
+    return rimg
+
+def concatenate_images(img, gt_img):
+    nChannels = len(gt_img.shape)
+    w = gt_img.shape[0]
+    h = gt_img.shape[1]
+    if nChannels == 3:
+        cimg = np.concatenate((img, gt_img), axis=1)
+    else:
+        gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
+        gt_img8 = img_float_to_uint8(gt_img)
+        gt_img_3c[:,:,0] = gt_img8
+        gt_img_3c[:,:,1] = gt_img8
+        gt_img_3c[:,:,2] = gt_img8
+        img8 = img_float_to_uint8(img)
+        cimg = np.concatenate((img8, gt_img_3c), axis=1)
+    return cimg
+
+
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches
+
+
+def extract_features(img):
+    feat_m = np.mean(img, axis=(0,1))
+    feat_v = np.var(img, axis=(0,1))
+    feat = np.append(feat_m, feat_v)
+    return feat
+
+
+# Extract 2-dimensional features consisting of average gray color as well as variance
+def extract_features_2d(img):
+    feat_m = np.mean(img)
+    feat_v = np.var(img)
+    feat = np.append(feat_m, feat_v)
+    return feat
+
+
+# Extract features for a given image
+def extract_img_features(filename, patch_size):
+    img = load_image(filename)
+    img_patches = img_crop(img, patch_size, patch_size)
+    X = np.asarray([ extract_features_2d(img_patches[i]) for i in range(len(img_patches))])
+    return X
+
+
+def patch_to_class(patch, threshold=0.25):
+    return 1 if np.sum(patch) > threshold else 0
+
+
+def polynomial_enhancement(x, deg):
+    """Horizontally concatenates a vector of 1, and the deg successive powers
+    of the x matrix
+    """
+    stacked_x = np.tile(x, deg)
+    power_vec = np.repeat(np.array(range(1, deg + 1)), x.shape[1])
+    return np.hstack((np.ones((stacked_x.shape[0], 1)), stacked_x ** power_vec))
+
+
+def label_to_img(imgwidth, imgheight, w, h, labels):
+    im = np.zeros([imgwidth, imgheight])
+    idx = 0
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            im[j:j+w, i:i+h] = labels[idx]
+            idx = idx + 1
+    return im
+
+
+def make_img_overlay(img, predicted_img):
+    w = img.shape[0]
+    h = img.shape[1]
+    color_mask = np.zeros((w, h, 3), dtype=np.uint8)
+    color_mask[:,:,0] = predicted_img*255
+
+    img8 = img_float_to_uint8(img)
+    background = Image.fromarray(img8, 'RGB').convert("RGBA")
+    overlay = Image.fromarray(color_mask, 'RGB').convert("RGBA")
+    new_img = Image.blend(background, overlay, 0.2)
+    return new_img
