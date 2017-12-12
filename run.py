bb93b93abe775bb995357610fbe8fd60285ea3a5
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-12-12 21:48:21 +0100

    Clean

--- run.py
+++ run.py
@@ -8,16 +8,17 @@ import torch.nn as nn
 from train import train_with_snapshots
 
 ROOT_DIR = 'training/'
-
 IMAGE_DIR = ROOT_DIR + 'images/'
+GT_DIR = ROOT_DIR + 'groundtruth/'
+KERNEL_SIDE = 3
+HIDDEN_SIZE = 12
+LEARNING_RATE = 0.1
+NITER = 10**6
 
 files = os.listdir(IMAGE_DIR)
 imgs = [load_image(IMAGE_DIR + file) for file in files]
 
-GT_DIR = ROOT_DIR + 'groundtruth/'
 gt_imgs = [load_image(GT_DIR + file) for file in files]
-
-KERNEL_SIDE = 3
 
 features = np.vstack([image_to_features(img, KERNEL_SIDE) for img in imgs])
 labels = [crop_groundtruth(gt, KERNEL_SIDE) for gt in gt_imgs]
@@ -27,16 +28,14 @@ X = torch.from_numpy(features)
 Y = torch.from_numpy(labels)
 
 model = nn.Sequential(
-    nn.Linear(KERNEL_SIDE**2 * 3, 12),
+    nn.Linear(KERNEL_SIDE**2 * 3, HIDDEN_SIZE),
     nn.Sigmoid(),
-    nn.Linear(12, 1),
+    nn.Linear(HIDDEN_SIZE, 1),
 )
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 costf = torch.nn.MSELoss()
 
-NITER = 10*6
-
-train_with_snapshots(X, Y, model, costf, optimizer, 10**6, 1000, 5000)
+train_with_snapshots(X, Y, model, costf, optimizer, NITER, 1000, 5000)
 
