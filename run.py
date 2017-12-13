faa7fd6c278d05a93a9ec3126c8c6f825a8fa622
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-12-13 10:12:24 +0100

    Move execution code to main

--- run.py
+++ run.py
@@ -15,18 +15,6 @@ HIDDEN_SIZE = 12
 LEARNING_RATE = 0.1
 NITER = 10**6
 
-files = os.listdir(IMAGE_DIR)
-imgs = [load_image(IMAGE_DIR + file) for file in files]
-
-gt_imgs = [load_image(GT_DIR + file) for file in files]
-
-features = np.vstack([image_to_features(img, KERNEL_SIDE) for img in imgs])
-labels = [crop_groundtruth(gt, KERNEL_SIDE) for gt in gt_imgs]
-labels = np.ravel(labels)
-
-X = torch.from_numpy(features)
-Y = torch.from_numpy(labels)
-
 model = nn.Sequential(
     nn.Linear(KERNEL_SIDE**2 * 3, HIDDEN_SIZE),
     nn.Sigmoid(),
@@ -37,5 +25,26 @@ optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 costf = torch.nn.MSELoss()
 
-train_with_snapshots(X, Y, model, costf, optimizer, NITER, 1000, 5000)
+if __name__ == '__main__':
+    files = os.listdir(IMAGE_DIR)
+
+    # Read images into numpy arrays
+    imgs = [load_image(IMAGE_DIR + file) for file in files]
+
+    # Read groundtruths into numpy arrays
+    gt_imgs = [load_image(GT_DIR + file) for file in files]
+
+    # Create matrix of features
+    features = np.vstack([image_to_features(img, KERNEL_SIDE) for img in imgs])
+
+    # Prepare labels
+    labels = [crop_groundtruth(gt, KERNEL_SIDE) for gt in gt_imgs]
+    labels = np.ravel(labels)
+
+    # Create pytorch tensors
+    X = torch.from_numpy(features)
+    Y = torch.from_numpy(labels)
+
+    # train
+    train_with_snapshots(X, Y, model, costf, optimizer, NITER, 1000, 5000)
 
