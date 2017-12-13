662d4deff8901a5b7ef590f60dca9331c061b687
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-12-13 21:55:00 +0100

    Modify parameters for grid search

--- grid_search_MLP.py
+++ grid_search_MLP.py
@@ -20,7 +20,7 @@ imgs = np.stack([saturate_hsv_img(rgb_to_hsv(load_image(IMAGE_DIR + file))) for 
 GT_DIR = ROOT_DIR + 'groundtruth/'
 gt_imgs = np.stack([load_image(GT_DIR + file) for file in files])
 
-CONV_KERNEL_SIDE = 3
+CONV_KERNEL_SIDE = 5
 PATCH_SIDE = 10
 
 patched_imgs = np.stack([patch_image(im, PATCH_SIDE) for im in imgs])
@@ -83,7 +83,7 @@ f = open('mlp_gridsearch.csv', 'w')
 notepad = csv.writer(f)
 for hidden_size in hidden_size_candidates:
     for lr in learning_rates_candidates:
-        f = generate_train_predict_MLP(lr, hidden_size, 30000)
+        f = generate_train_predict_MLP(lr, hidden_size, 20000)
         res = cross_validation(len(imgs), 5, f)
         print([hidden_size, lr, res])
         notepad.writerow([hidden_size, lr, res])
