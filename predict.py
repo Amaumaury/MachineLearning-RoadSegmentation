b02cc5b14364d407d804b1bdefc4c6c6b78529f5
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-12-17 08:10:47 +0000

    Hack predict to increment image size, patch and predict

--- predict.py
+++ predict.py
@@ -12,11 +12,16 @@ WINDOW_SIZE = 71
 for file in test_files:
     filename = '{}{}/{}.png'.format(TEST_DIR, file, file)
     img = load_image(filename)
-    windows = image_to_features(img, WINDOW_SIZE, True)
+    img_m = reflect_padding(img, 1)
+    img_m = patch_image(img, 10)
+    windows = image_to_features(img_m, WINDOW_SIZE, True)
     print('Read and windowed', file)
     preds = m.predict(windows)
     print('Generated predictions')
     preds = (preds[:,1] > preds[:,0]) * 1
-    preds = np.reshape(preds, img.shape[:2])
+    preds = np.reshape(preds, img_m.shape[:2])
+    preds = unpatch(preds, 10)
+    preds = preds[1:-1, 1:-1]
+    assert preds.shape == img.shape[:2]
     np.save(file, preds)
     print('Saved array')
