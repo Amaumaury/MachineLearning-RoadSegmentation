20d8a23375f9dcfadc5c545f5c494f9de4bf53ef
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2017-12-17 10:27:09 +0100

    Implement renaming

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.models import load_model
-from best_utils import *
+from preprocessing import *
 import os, sys
 
 m = load_model('trained_model.hdf5')
@@ -14,7 +14,7 @@ for file in test_files:
     img = load_image(filename)
     img_m = reflect_padding(img, 1)
     img_m = patch_image(img, 10)
-    windows = image_to_features(img_m, WINDOW_SIZE, True)
+    windows = image_to_neighborhoods(img_m, WINDOW_SIZE, True)
     print('Read and windowed', file)
     preds = m.predict(windows)
     print('Generated predictions')
@@ -25,3 +25,4 @@ for file in test_files:
     assert preds.shape == img.shape[:2]
     np.save(file, preds)
     print('Saved array')
+
